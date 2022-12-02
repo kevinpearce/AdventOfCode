7a336b8784b406a4afce6581160ca7d45077cc76
Kevin Pearce <kevinjohnpearce@googlemail.com>
2022-12-02 17:28:55 +0000

    feat: add day1 solution

--- 2022/Day1/Program.cs
+++ 2022/Day1/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
 
+var Elves = new List<int>();
+
+var index = 0;
+
+foreach (string line in File.ReadLines("./input.txt"))
+{
+    var calories = 0;
+    Int32.TryParse(line, out calories);
+
+    if (calories == 0)
+    {
+        index ++;
+        continue;
+    }
+
+    if (Elves.Count() == index)
+    {
+        Elves.Add(calories);
+        continue;
+    }
+
+    Elves[index] += calories;
+}
+
+Console.WriteLine($"Part 1 : {HighestCalorieTotal(Elves)}"); //71124
+
+Console.WriteLine($"Part 2 : {HighestCalorieTotal(Elves, 3)}"); //204639
+
+int HighestCalorieTotal(List<int> caloriesPerElf, int numberOfElves = 1)
+{
+    var sum = 0;
+    var localList = new List<int>(caloriesPerElf);
+
+    for (var i = 0; i < numberOfElves; i++)
+    {
+        sum += localList.Max();
+
+        var maxIndex = localList.IndexOf(localList.Max());
+
+        localList.RemoveAt(maxIndex);
+    }
+
+    return sum;
+}
