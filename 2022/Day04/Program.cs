0d43ed1d56adad9df48d3f7efb92d696d604a4e1
Kevin Pearce <kevinjohnpearce@googlemail.com>
2022-12-04 19:46:40 +0000

    feat: add Day04 solution

--- 2022/Day04/Program.cs
+++ 2022/Day04/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var totalOverlappingRanges = 0;
+var partialOverlapping = 0;
+
+foreach (string line in File.ReadLines("./input.prod"))
+{
+    totalOverlappingRanges += RangeInRange(line);
+    partialOverlapping += PartialRangeInRange(line);
+}
+
+Console.WriteLine($"Part 1 : {totalOverlappingRanges}"); // 657
+Console.WriteLine($"Part 2 : {partialOverlapping}"); // 938
+
+int RangeInRange (string line)
+{
+    var range = line.Split(',');
+
+    var aSplit = range[0].Split('-');
+    int startA = Int32.Parse(aSplit[0]);
+    int endA = Int32.Parse(aSplit[1]);
+
+    var bSplit = range[1].Split('-');
+    int startB = Int32.Parse(bSplit[0]);
+    int endB = Int32.Parse(bSplit[1]);
+
+    if (startA <= startB && endA >= endB)
+    {
+        return 1;
+    }
+
+    if (startB <= startA && endB >= endA)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int PartialRangeInRange (string line)
+{
+    var range = line.Split(',');
+
+    var aSplit = range[0].Split('-');
+    int startA = Int32.Parse(aSplit[0]);
+    int endA = Int32.Parse(aSplit[1]);
+
+    var bSplit = range[1].Split('-');
+    int startB = Int32.Parse(bSplit[0]);
+    int endB = Int32.Parse(bSplit[1]);
+
+    if (startA <= startB && endA >= startB)
+    {
+        return 1;
+    }
+
+    if (startB <= startA && endB >= startA)
+    {
+        return 1;
+    }
+
+    return 0;
+}
