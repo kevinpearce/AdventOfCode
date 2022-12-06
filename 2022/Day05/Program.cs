aa467865e996ea4a92a44422e814f4c9cf5a5179
Kevin Pearce <kevinjohnpearce@googlemail.com>
2022-12-06 19:38:10 +0000

    feat: add Day05 solution

--- 2022/Day05/Program.cs
+++ 2022/Day05/Program.cs
@@ -47,15 +47,23 @@ foreach (string line in instructionData)
     var from = Int32.Parse(instruction[1]) - 1;
     var to = Int32.Parse(instruction[2]) - 1;
 
+    Stack<char> tempStorage = new Stack<char>();
+
     for (int i = 0; i < numberOfMoves; i++)
     {
-        var crate = stacks[from].Pop();
+        var tempCrate = stacks[from].Pop();
 
-        stacks[to].Push(crate);
+        tempStorage.Push(tempCrate);
     }
+
+    while (tempStorage.Count > 0)
+    {
+        stacks[to].Push(tempStorage.Pop());
+    }
+
 }
 
 foreach (Stack<char> endStack in stacks)
 {
-    Console.WriteLine(endStack.Peek()); // PTWLTDSJV
+    Console.WriteLine(endStack.Peek()); // Part1 : PTWLTDSJV, Part2 : WZMFVGGZP
 }
