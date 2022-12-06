bd6891e870efb64284c6368f7cf09a71477b6e70
Kevin Pearce <kevinjohnpearce@googlemail.com>
2022-12-06 19:25:57 +0000

    feat: add Day05-P1 solution

--- 2022/Day05/Program.cs
+++ 2022/Day05/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] theFile = File.ReadAllLines("./input.prod");
+
+List<string> rawStackData = new List<string>();
+List<string> instructionData = new List<string>();
+List<Stack<char>> stacks = new List<Stack<char>>();
+
+foreach (string line in theFile)
+{
+    if (line == string.Empty) continue;
+
+    if (line[0] == 'm')
+    {
+        instructionData.Add(line);
+    }
+    else
+    {
+        rawStackData.Add(line);
+    }
+}
+
+foreach (string line in Enumerable.Reverse(rawStackData).Skip(1))
+{
+    var stack = 0;
+
+    for (int i = 0; i < line.Length; i += 4)
+    {
+        if (line[i] == '[')
+        {
+            if (stacks.ElementAtOrDefault(stack) == null)
+            {
+                stacks.Add(new Stack<char>());
+            }
+
+            stacks[stack].Push(line[i+1]);
+        }
+        stack ++;
+    }
+}
+
+foreach (string line in instructionData)
+{
+    string[] separators = { "move ", " from ", " to " };
+
+    var instruction = line.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+
+    var numberOfMoves = Int32.Parse(instruction[0]);
+    var from = Int32.Parse(instruction[1]) - 1;
+    var to = Int32.Parse(instruction[2]) - 1;
+
+    for (int i = 0; i < numberOfMoves; i++)
+    {
+        var crate = stacks[from].Pop();
+
+        stacks[to].Push(crate);
+    }
+}
+
+foreach (Stack<char> endStack in stacks)
+{
+    Console.WriteLine(endStack.Peek()); // PTWLTDSJV
+}
