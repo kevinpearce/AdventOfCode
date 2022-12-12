13338eb35ae1d9150362f59dec0b27ed7b40a17f
Kevin Pearce <kevinjohnpearce@googlemail.com>
2022-12-12 22:17:42 +0000

    feat: add Day08 pt1 solution - needs refactor

--- 2022/Day08/Program.cs
+++ 2022/Day08/Program.cs
@@ -1,13 +1,83 @@
 ﻿string[] theFile = File.ReadAllLines("./input.test");
 
-// index through inner trees
-for (int y=1; y < theFile.Length - 1; y++)
-{
-    for (int x=1; x < theFile[y].Length - 1; x++)
-    {
-        var height = theFile[y][x];
+int[,] results = new int[theFile.Length, theFile[0].Length];
 
-        // Pass to check routine here?
-        Console.WriteLine(height);
+char currentCell = '/';
+
+// LEFT walk
+for (int y = 0; y < theFile.Length; y++)
+{
+    for (int x = 0; x < theFile[0].Length; x++)
+    {
+        CheckHeight(y, x);
+    }
+    currentCell = '/';
+}
+
+// RIGHT walk
+for (int y = theFile.Length - 1; y >= 0; y--)
+{
+    for (int x = theFile[0].Length - 1; x >= 0; x--)
+    {
+        CheckHeight(y, x);
+    }
+    currentCell = '/';
+}
+
+// TOP walk 
+for (int x = 0; x < theFile[0].Length; x++)
+{
+    for (int y = 0; y < theFile.Length; y++)
+    {
+        CheckHeight(y, x);
+    }
+    currentCell = '/';
+}
+
+// BASE walk
+for (int x = theFile[0].Length - 1; x >= 0; x--)
+{
+    for (int y = theFile.Length - 1; y >= 0; y--)
+    {
+        CheckHeight(y, x);
+    }
+    currentCell = '/';
+}
+
+// print raw data
+foreach (string line in theFile)
+{
+    Console.WriteLine(line);
+}
+
+// whitespace
+Console.WriteLine();
+
+// print result array
+for (int i = 0; i < results.GetLength(0); i++)
+{
+    for (int j = 0; j < results.GetLength(1); j++)
+    {
+        Console.Write(results[i,j]);
+    }
+    Console.WriteLine();
+}
+
+// whitespace
+Console.WriteLine();
+
+int sum = results.Cast<int>().Sum();
+Console.WriteLine($"Part 1 : {sum}"); // Part1: 1851
+
+void CheckHeight (int y, int x)
+{
+    var height = theFile[y][x];
+
+    if (theFile[y][x] > currentCell)
+    {
+        results[y, x] = 1;
+        currentCell = height;
     }
 }
+
+// Part2 : the tree that can see the most zeroes???
