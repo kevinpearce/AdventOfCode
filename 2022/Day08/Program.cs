dc51b845505b9b3b081ce48468cc3b3a8448682d
Kevin Pearce <42847716+kevinpearce@users.noreply.github.com>
2022-12-12 16:30:04 +0000

    feat: add Day08 opening loop

--- 2022/Day08/Program.cs
+++ 2022/Day08/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] theFile = File.ReadAllLines("./input.test");
+
+// index through inner trees
+for (int y=1; y < theFile.Length - 1; y++)
+{
+    for (int x=1; x < theFile[y].Length - 1; x++)
+    {
+        var height = theFile[y][x];
+
+        // Pass to check routine here?
+        Console.WriteLine(height);
+    }
+}
