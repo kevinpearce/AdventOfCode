6cf755b346f44eb5d934c8ffa6aab085643e2cad
Kevin Pearce <kevinjohnpearce@googlemail.com>
2022-12-14 22:00:19 +0000

    feat: add Day 8 Pt2 solution - begging for a refactor

--- 2022/Day08/Program.cs
+++ 2022/Day08/Program.cs
@@ -1,7 +1,11 @@
-﻿string[] theFile = File.ReadAllLines("./input.test");
+﻿string[] theFile = File.ReadAllLines("./input.prod");
 
 int[,] results = new int[theFile.Length, theFile[0].Length];
-int[,] treesVisibleCount = new int[theFile.Length, theFile[0].Length];
+int[,] treesVisibleLeft = new int[theFile.Length, theFile[0].Length];
+int[,] treesVisibleRight = new int[theFile.Length, theFile[0].Length];
+int[,] treesVisibleTop = new int[theFile.Length, theFile[0].Length];
+int[,] treesVisibleBase = new int[theFile.Length, theFile[0].Length];
+int[,] totalVisible = new int[theFile.Length, theFile[0].Length];
 
 char currentCell = '/';
 int currentCellVisibilty = 0;
@@ -12,75 +16,60 @@ for (int y = 0; y < theFile.Length; y++)
     for (int x = 0; x < theFile[0].Length; x++)
     {
         CheckHeight(y, x);
-        CheckVisibiltyLeft(y, x);
+
+        //Part 2 addition
+        if (!VerifyIfEdge(y, x))
+        {
+            CheckVisibilty(y, x);
+        }
     }
     currentCell = '/';
 }
 
-// // RIGHT walk
-// for (int y = theFile.Length - 1; y >= 0; y--)
-// {
-//     for (int x = theFile[0].Length - 1; x >= 0; x--)
-//     {
-//         CheckHeight(y, x);
-//     }
-//     currentCell = '/';
-// }
-
-// // TOP walk 
-// for (int x = 0; x < theFile[0].Length; x++)
-// {
-//     for (int y = 0; y < theFile.Length; y++)
-//     {
-//         CheckHeight(y, x);
-//     }
-//     currentCell = '/';
-// }
-
-// // BASE walk
-// for (int x = theFile[0].Length - 1; x >= 0; x--)
-// {
-//     for (int y = theFile.Length - 1; y >= 0; y--)
-//     {
-//         CheckHeight(y, x);
-//     }
-//     currentCell = '/';
-// }
-
-// print raw data
-foreach (string line in theFile)
+// RIGHT walk
+for (int y = theFile.Length - 1; y >= 0; y--)
 {
-    Console.WriteLine(line);
-}
-
-// whitespace
-Console.WriteLine();
-
-// print tree heights array
-for (int i = 0; i < treesVisibleCount.GetLength(0); i++)
-{
-    for (int j = 0; j < treesVisibleCount.GetLength(1); j++)
+    for (int x = theFile[0].Length - 1; x >= 0; x--)
     {
-        Console.Write(treesVisibleCount[i,j]);
+        CheckHeight(y, x);
     }
-    Console.WriteLine();
+    currentCell = '/';
 }
 
-// print result array
-// for (int i = 0; i < results.GetLength(0); i++)
-// {
-//     for (int j = 0; j < results.GetLength(1); j++)
-//     {
-//         Console.Write(results[i,j]);
-//     }
-//     Console.WriteLine();
-// }
+// TOP walk 
+for (int x = 0; x < theFile[0].Length; x++)
+{
+    for (int y = 0; y < theFile.Length; y++)
+    {
+        CheckHeight(y, x);
+    }
+    currentCell = '/';
+}
 
-// whitespace
-// Console.WriteLine();
+// BASE walk
+for (int x = theFile[0].Length - 1; x >= 0; x--)
+{
+    for (int y = theFile.Length - 1; y >= 0; y--)
+    {
+        CheckHeight(y, x);
+    }
+    currentCell = '/';
+}
 
-// int sum = results.Cast<int>().Sum();
-// Console.WriteLine($"Part 1 : {sum}"); // Part1: 1851
+// final tree visibilty calculator
+for (int i = 0; i < treesVisibleBase.GetLength(0); i++)
+{
+    for (int j = 0; j < treesVisibleBase.GetLength(1); j++)
+    {
+        totalVisible[i,j] = treesVisibleBase[i,j] * treesVisibleLeft[i,j] * treesVisibleRight[i,j] * treesVisibleTop[i,j];
+    }
+}
+
+int sum = results.Cast<int>().Sum();
+Console.WriteLine($"Part 1 : {sum}"); // Part1: 1851
+
+int max = totalVisible.Cast<int>().Max();
+Console.WriteLine($"Part 2 : {max}"); // Part2: 574080
 
 void CheckHeight (int y, int x)
 {
@@ -93,29 +82,107 @@ void CheckHeight (int y, int x)
     }
 }
 
-// Part2 : the tree that can see the most zeroes???
+void CheckVisibilty (int y, int x)
+{
+    CheckVisibiltyLeft(y, x);
+    CheckVisibiltyRight(y, x);
+    CheckVisibiltyTop(y, x);
+    CheckVisibiltyBase(y, x);
+}
 
 void CheckVisibiltyLeft (int y, int x)
-{
-    if (VerifyIfEdge(y, x))
+{   
+    if (x > 0)
     {
-        return;
-    }
-    else if (theFile[x][y] > theFile[x-1][y])
-    {
-        currentCellVisibilty ++;
-        CheckVisibiltyLeft(y, x-1);
+        for (int i = 1; x-i >= 0; i++)
+        {
+            if (theFile[y][x] <= theFile[y][x-i])
+            {
+                currentCellVisibilty ++;
+                break;
+            }
+            else
+            {
+                currentCellVisibilty ++;
+            }
+        }
     }
 
-    treesVisibleCount[y, x] += currentCellVisibilty;
+    treesVisibleLeft[y, x] += currentCellVisibilty;
+    currentCellVisibilty = 0;
+}
+
+void CheckVisibiltyRight (int y, int x)
+{   
+    if (x < theFile[0].Length)
+    {
+        for (int i = 1; x+i < theFile[0].Length; i++)
+        {
+            if (theFile[y][x] <= theFile[y][x+i])
+            {
+                currentCellVisibilty ++;
+                break;
+            }
+            else
+            {
+                currentCellVisibilty ++;
+            }
+        }
+    }
+
+    treesVisibleRight[y, x] += currentCellVisibilty;
+    currentCellVisibilty = 0;
+}
+
+void CheckVisibiltyTop (int y, int x)
+{   
+    if (y > 0)
+    {
+        for (int i = 1; y-i >= 0; i++)
+        {
+            if (theFile[y][x] <= theFile[y-i][x])
+            {
+                currentCellVisibilty ++;
+                break;
+            }
+            else
+            {
+                currentCellVisibilty ++;
+            }
+        }
+    }
+
+    treesVisibleTop[y, x] += currentCellVisibilty;
+    currentCellVisibilty = 0;
+}
+
+void CheckVisibiltyBase (int y, int x)
+{   
+    if (y < theFile.Length)
+    {
+        for (int i = 1; y+i < theFile.Length; i++)
+        {
+            if (theFile[y][x] <= theFile[y+i][x])
+            {
+                currentCellVisibilty ++;
+                break;
+            }
+            else
+            {
+                currentCellVisibilty ++;
+            }
+        }
+    }
+
+    treesVisibleBase[y, x] += currentCellVisibilty;
     currentCellVisibilty = 0;
 }
 
 bool VerifyIfEdge (int y, int x)
 {
     if (
-        x == 0 || x == theFile[0].Length ||
-        y == 0 || y == theFile.Length) 
+        x == 0 || x == theFile[0].Length - 1 ||
+        y == 0 || y == theFile.Length - 1) 
     {
         return true;
     }
