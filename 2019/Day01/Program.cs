638e44f943227854d3cc9cf33517ecc1e9a6ec16
Kevin Pearce <42847716+kevinpearce@users.noreply.github.com>
2022-12-21 16:56:20 +0000

    feat: add 2019 directory and Day01 solution

---
+++ 2019/Day01/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] theFile = File.ReadAllLines("./input.prod");
+
+int sumOfFuel = 0;
+int sumOfAddedFuel = 0;
+
+foreach (string line in theFile)
+{
+    sumOfFuel += Calculator(Int32.Parse(line)); //part1
+
+    Calculator(Int32.Parse(line), true); //part2
+}
+
+Console.WriteLine($"Part1 : {sumOfFuel}"); //part1 = 3399947
+Console.WriteLine($"Part2 : {sumOfAddedFuel}"); //part2 = 5097039
+
+int Calculator (int input, bool recursive = false)
+{
+    int result = input / 3 - 2;
+
+    if (result > 0 && recursive)
+    {
+        sumOfAddedFuel += result;
+        return Calculator(result, true);
+    }
+
+    return result;
+}
